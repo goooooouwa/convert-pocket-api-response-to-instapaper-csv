a2640374029963e3d2514e264077e10598a5eb3a
Shunfa Xu <gouwa5700@gmail.com>
2018-09-10 16:42:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## How to use
+
+``
+
+## Caveats
+First article in a non-exist Folder will go into Favorites folder as the folder being created, while the rest will go to the newly created folder. So you will end up with some articles not categoried.
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const pocketApiResponse = require('./pocket-response.json');
+
+let result = 'URL,Title,Selection,Folder';
+let items = pocketApiResponse.list;
+for (var itemId in items) {
+    if (items.hasOwnProperty(itemId)) {
+        const item = items[itemId];
+        const tag = item.hasOwnProperty('tags') ? Object.keys(item.tags)[0].replace('&amp;','').replace('/','') : 'Home';
+        result += `
+"${item.given_url}","${item.given_title.replace('&amp;','&')}","","${tag.charAt(0).toUpperCase() + tag.substr(1)}"`;
+    }
+}
+
+console.log(result);
